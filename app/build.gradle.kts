8cc8eb4e0f7a97e28a897470119ce4e94140bd01
PAiN <bibeks337@gmail.com>
2024-09-24 13:20:29 +0545

    addition of fragments and other pages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
